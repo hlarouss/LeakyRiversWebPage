0d359f92d251f8bd4d99e654e9a01b0fc0d1e36c
Norbert Bonnici <norbert@bonnici.co>
2016-04-23 22:39:38 +0200

    whole data

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   	  // We are now going to read a document from our cloudant database. We are going
   	  // to retrieve the value associated with myKey from the body of the document.
     	  //The SAG PHP library takes care of all the gory details and only retrieves the value.
-  	  $resp = $sag->get('_design/nameview/_view/nameview')->body->value;
+  	  $resp = $sag->get('_design/nameview/_view/nameview')->body;
     //   }
 
 }
