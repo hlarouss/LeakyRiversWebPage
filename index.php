748c9145163aaecf822bfb381b5a7dd071f2d47d
Norbert Bonnici <norbert@bonnici.co>
2016-04-23 19:57:40 +0200

    bye bye

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 			<td style='width: 30%;'><img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello world!"; ?>
+				<h1 id = "message"><?php echo "Bye world!"; ?>
 </h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>. Get started by reading our <a
 				href="https://www.ng.bluemix.net/docs/#runtimes/php/index.html">documentation</a>
