4c1e8e31710d671067a197117cdcd93b5bef9aed
Norbert Bonnici <norbert@bonnici.co>
2016-04-24 12:44:07 +0200

    local js and css

--- index.php
+++ index.php
@@ -32,8 +32,6 @@
 
  ?>
 
-
-
 <!DOCTYPE HTML>
 
 <html>
@@ -43,8 +41,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
-        <link rel="stylesheet" href="http://openlayers.org/en/v3.15.1/css/ol.css" type="text/css">
-        <script src="http://openlayers.org/en/v3.15.1/build/ol.js"></script>
+        <link rel="stylesheet" href="assets/css/ol.css" type="text/css">
+        <script src="assets/js/ol.js"></script>
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
